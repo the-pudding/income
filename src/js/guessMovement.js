437cdb6e5c5b95e5072b2495d695ca06050ecb68
Alice Feng <feng.a.g@gmail.com>
2020-03-26 21:46:40 -0400

    Fix bug where bars summed to 100 but message was displaying there were still more families to allocate

--- src/js/guessMovement.js
+++ src/js/guessMovement.js
@@ -88,17 +88,20 @@ function brushMove() {
     // if user drags bar such that the sum of all the bars is greater than 100, prevent them from continuing to drag
     const remainder = d3.sum(guessData.filter(d => d.levels !== brushedLevel), d => d.share);
 	if(newShare + remainder > 100) {
+		guessData[brushedLevel].share = 100 - remainder;
 		d3.select(this).call($brush.move, function(d) { return [100 - remainder, 0].map(scaleY); });
 		// console.log(guessData);
 		$instruction2.style('visibility', 'visible');
 	}
-    // else, update the dataset with the new value
+    // else, update the dataset with the new value and snap the bar to the nearest integer
     else {
 	    guessData[brushedLevel].share = newShare;
-	    d3.select(this).call($brush.move, function(d) { return [newShare, 0].map(scaleY); }); // snap the bar to the nearest integer
+	    d3.select(this).call($brush.move, function(d) { return [newShare, 0].map(scaleY); });
 		// const sum = d3.sum(guessData, d => d.share);
-		updateWarningMsg();
     }
+
+	updateWarningMsg();
+
 }
 
 function brushEnd() {
