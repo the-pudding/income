603612d16c886bcc3f7870c78ad1942126b092ba
Alice Feng <feng.a.g@gmail.com>
2020-04-08 21:37:42 -0400

    Begin drawing a line on the chart

--- src/js/familyLines.js
+++ src/js/familyLines.js
@@ -20,7 +20,16 @@ const scaleY_line = d3.scaleLinear()
 
 // const scaleX_hist =
 // const scaleY_hist =
-// const colorScale =
+
+const colorScale = d3.scaleOrdinal()
+	.domain([1, 2, 3, 4, 5])
+	.range(["#EB5757", "#EB5757", "#F2C94C", "#6FCF97", "#6FCF97"]);
+
+const line = d3.line()
+    // .defined(function(d) { return !isNaN(d.enrollment) && d.enrollment >= 0; })
+    .x(d => scaleX_line(d.year))
+    .y(d => scaleY_line(d.pctile))
+    .curve(d3.curveStepAfter);
 
 // console.log($guess__container.node().offsetWidth, $guess__container.node().offsetHeight)
 
@@ -30,5 +39,32 @@ let $family__vis = $family__svg.attr('width', width + margin.left + margin.right
 	.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
 loadData('line_chart_data.csv').then(result => {
-	console.log(result);
+	// console.log(result);
+	// nest data because that's the structure d3 needs to make line charts
+	let dataByFamily = d3.nest()
+		.key(d => d.id)
+		.entries(result);
+
+	// console.log(dataByFamily);
+
+	// append axis - this will need to be changed later to match mockup
+	$family__vis.append('g')
+		.attr('class', 'axis axis--y')
+		.call(d3.axisLeft(scaleY_line));
+
+	$family__vis.append('g')
+		.attr('class', 'axis axis--x')
+		.attr('transform', 'translate(0,' + height + ')')
+		.call(d3.axisBottom(scaleX_line));
+
+	// draw first line
+	$family__vis.selectAll('.line')
+		.data(dataByFamily.filter((d, i) => i === 0))
+		.enter()
+		.append('path')
+		.attr('class', 'line')
+		.attr('d', d => line(d.values))
+		.style('stroke', '#000')
+		.style('fill', 'none');
+
 }).catch(console.error);
