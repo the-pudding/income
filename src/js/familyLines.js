b0b8d134fddb05d744efeb2d91663b59fd6425fd
Alice Feng <feng.a.g@gmail.com>
2020-04-09 21:10:16 -0400

    Add in background and label to chart. Also make lines crisper

--- src/js/familyLines.js
+++ src/js/familyLines.js
@@ -5,13 +5,13 @@ const $family__container = $section.select('.family__figure');
 const $family__svg = $family__container.select('svg');
 
 // dimensions
-const margin = {top: 20, bottom: 40, left: 38, right: 10};
+const margin = {top: 20, bottom: 40, left: 140, right: 10};
 let width = $family__container.node().offsetWidth - margin.left - margin.right;
 let height = $family__container.node().offsetHeight - margin.top - margin.bottom;
 
 // scales
 const scaleX_line = d3.scaleLinear()
-	.domain([0, 40])
+	// .domain([0, 40])
 	.range([0, width]);
 
 const scaleY_line = d3.scaleLinear()
@@ -22,8 +22,8 @@ const scaleY_line = d3.scaleLinear()
 // const scaleY_hist =
 
 const colorScale = d3.scaleOrdinal()
-	.domain([1, 2, 3, 4, 5])
-	.range(["#EB5757", "#EB5757", "#F2C94C", "#6FCF97", "#6FCF97"]);
+	.domain(["Lower", "Lower Middle", "Middle", "Upper Middle", "Upper"])
+	.range(["#f9cdce", "#fbdddd", "#fcf5db", "#e1f5ea", "#d4f1df"]);
 
 const line = d3.line()
     // .defined(function(d) { return !isNaN(d.enrollment) && d.enrollment >= 0; })
@@ -38,27 +38,54 @@ let $family__vis = $family__svg.attr('width', width + margin.left + margin.right
 	.append('g')
 	.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
+// color plot background based on quintile
+let $background = $family__vis.append('g')
+	.selectAll('.quintileBlock')
+	.data(colorScale.domain())
+	.enter()
+	.append('g')
+	.attr('class', 'quintileBlock');
+
+$background.append('rect')
+	.attr('class', 'quintileRect')
+	.attr('x', 0)
+	.attr('y', (d, i) => scaleY_line((i + 1) * 20))
+	.attr('width', width)
+	.attr('height', height / 5)
+	.style('fill', d => colorScale(d));
+
+// add "y axis"
+$background.append('text')
+	.attr('class', 'quintileLabel')
+	.attr('x', -5)
+	.attr('y', (d, i) => scaleY_line(i * 20))
+	.attr('dy', '-.5em')
+	.text(d => d);
+
+let $lines = $family__vis.append('g');
+
 loadData('line_chart_data.csv').then(result => {
-	// console.log(result);
+	console.log(result);
 	// nest data because that's the structure d3 needs to make line charts
 	let dataByFamily = d3.nest()
 		.key(d => d.id)
 		.entries(result);
 
+	scaleX_line.domain([0, d3.max(result, d => +d.year)]).nice();
 	// console.log(dataByFamily);
 
-	// append axis - this will need to be changed later to match mockup
-	$family__vis.append('g')
-		.attr('class', 'axis axis--y')
-		.call(d3.axisLeft(scaleY_line));
+	// append axis for debugging purposes
+	// $family__vis.append('g')
+	// 	.attr('class', 'axis axis--y')
+	// 	.call(d3.axisLeft(scaleY_line));
 
-	$family__vis.append('g')
-		.attr('class', 'axis axis--x')
-		.attr('transform', 'translate(0,' + height + ')')
-		.call(d3.axisBottom(scaleX_line));
+	// $family__vis.append('g')
+	// 	.attr('class', 'axis axis--x')
+	// 	.attr('transform', 'translate(0,' + height + ')')
+	// 	.call(d3.axisBottom(scaleX_line));
 
 	// draw first line
-	$family__vis.selectAll('.line')
+	$lines.selectAll('.line')
 		.data(dataByFamily.filter((d, i) => i < 20)) // just plot a subset of the data for now
 		.enter()
 		.append('path')
@@ -69,7 +96,7 @@ loadData('line_chart_data.csv').then(result => {
 	let fam_num = 1;
 
 	let t = d3.interval(function(elapsed) {
-		$family__vis.select('.line.family_' + fam_num)
+		$lines.select('.line.family_' + fam_num)
 			.transition(800)
 			.style('opacity', 1);
 
